ec4e71a70214bd61739ec4999be4dfbea6b399b1
rd3v <blackscreen405@yahoo.co.id>
2019-06-12 21:42:01 +0800

    Ubah 'base_url' dari 'localhost' menjadi 'https://projects.upanastudio.com/ptssm/app/'

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ptssm/app2';
-# $config['base_url'] = 'https://projects.upanastudio.com/ptssm/app/';
+// $config['base_url'] = 'http://localhost/ptssm/app2';
+$config['base_url'] = 'https://projects.upanastudio.com/ptssm/app/';
 
 /*
 |--------------------------------------------------------------------------
